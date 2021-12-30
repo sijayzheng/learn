4231480bf107d4509e00122eac7490063e89cd8b
郑世杰 <sijaycheng@163.com>
2021-12-30 17:21:03 +0800

    更新代码

--- learnMyBatis/mybatis01/src/main/java/zheng/sijay/mapper/UserMapper.java
+++ learnMyBatis/mybatis01/src/main/java/zheng/sijay/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package zheng.sijay.mapper;
 
+import org.apache.ibatis.annotations.Delete;
 import zheng.sijay.pojo.UserPO;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author 郑世杰
@@ -22,4 +24,33 @@ public interface UserMapper {
      * @return 用户
      */
     UserPO getUserById(int id);
+
+    List<UserPO> listUserByName(String name);
+
+    /**
+     * 添加
+     *
+     * @param user
+     * @return
+     */
+    int addUser(UserPO user);
+
+    int addOneUser(Map<String, Object> map);
+
+    /**
+     * 修改
+     *
+     * @param user
+     * @return
+     */
+    int updateUser(UserPO user);
+
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
+    @Delete("delete from user where id=#{id}")
+    int deleteUser(int id);
 }
--- learnMyBatis/mybatis01/src/test/java/zheng/sijay/UserMapperTest.java
+++ learnMyBatis/mybatis01/src/test/java/zheng/sijay/UserMapperTest.java
@@ -3,7 +3,11 @@ package zheng.sijay;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import zheng.sijay.mapper.UserMapper;
+import zheng.sijay.pojo.UserPO;
 import zheng.sijay.utils.MyBatisUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 郑世杰
@@ -26,6 +30,20 @@ public class UserMapperTest {
     }
 
     @Test
+    public void listUserByName() {
+        // 从 SqlSessionFactory 中获取 SqlSession
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession()) {
+            // 获取接口
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            // 执行SQL
+            // 新写法
+            mapper.listUserByName("%u%").forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void getUserById() {
         // 从 SqlSessionFactory 中获取 SqlSession
         try (SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession()) {
@@ -33,6 +51,67 @@ public class UserMapperTest {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
             // 执行SQL
             System.out.println(mapper.getUserById(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addUser() {
+        // 从 SqlSessionFactory 中获取 SqlSession
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession()) {
+            // 获取接口
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            UserPO po = new UserPO(11, "jan", "123456");
+            // 执行SQL
+            System.out.println(mapper.addUser(po));
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void updateUser() {
+        // 从 SqlSessionFactory 中获取 SqlSession
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession()) {
+            // 获取接口
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            UserPO po = new UserPO(10, "tom L", "654321");
+            // 执行SQL
+            System.out.println(mapper.updateUser(po));
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void deleteUser() {
+        // 从 SqlSessionFactory 中获取 SqlSession
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession()) {
+            // 获取接口
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            // 执行SQL
+            System.out.println(mapper.deleteUser(3));
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addOneUser() {
+        // 从 SqlSessionFactory 中获取 SqlSession
+        try (SqlSession sqlSession = MyBatisUtils.getSqlSessionFactory().openSession()) {
+            // 获取接口
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            Map<String, Object> map = new HashMap<>();
+            map.put("userId", 13);
+            map.put("user_name", "usernames");
+            // 执行SQL
+            System.out.println(mapper.addOneUser(map));
+            sqlSession.commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
