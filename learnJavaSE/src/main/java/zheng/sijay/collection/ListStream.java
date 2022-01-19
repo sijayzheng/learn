a0766ab7dbc5ea9a1ea373c0d37005d6766db38d
sijayzheng <sijaycheng@163.com>
2022-01-19 22:25:11 +0800

    更新项目结构，添加新模块

---
+++ learnJavaSE/src/main/java/zheng/sijay/collection/ListStream.java
@@ -0,0 +1,11 @@
+package zheng.sijay.collection;
+
+/**
+ * @author 郑世杰
+ * @date 2022/1/19 22:21
+ */
+public class ListStream {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ learnJavaSE/src/main/java/zheng/sijay/string/StringUtil.java
@@ -0,0 +1,8 @@
+package zheng.sijay.string;
+
+/**
+ * @author 郑世杰
+ * @date 2022/1/19 22:22
+ */
+public class StringUtil {
+}
