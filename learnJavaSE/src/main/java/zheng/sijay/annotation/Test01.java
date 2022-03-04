8ff4d1556790d83b0ad0d01564a54eec76bd31a3
郑世杰 <sijaycheng@163.com>
2022-03-04 17:57:31 +0800

    更新代码

---
+++ learnJavaSE/src/main/java/zheng/sijay/annotation/Test01.java
@@ -0,0 +1,14 @@
+package zheng.sijay.annotation;
+
+/**
+ * @author 郑世杰
+ */
+@SuppressWarnings("all")
+public class Test01 extends Object {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+}
