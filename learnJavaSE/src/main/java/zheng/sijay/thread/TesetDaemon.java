fad79a462e015d7f7daba9a545151848b0ac66a9
sijayzheng <sijaycheng@163.com>
2022-03-10 23:16:10 +0800

    更新代码

---
+++ learnJavaSE/src/main/java/zheng/sijay/thread/TesetDaemon.java
@@ -0,0 +1,37 @@
+package zheng.sijay.thread;
+
+/**
+ * @author 郑世杰
+ * @date 2022/3/10 21:32
+ */
+public class TesetDaemon {
+    public static void main(String[] args) {
+        God god = new God();
+        People people = new People();
+        Thread thread = new Thread(god);
+        thread.setDaemon(true);
+        thread.start();
+        Thread thread1 = new Thread(people);
+        thread1.start();
+    }
+}
+
+class People implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("hello world");
+        for (int i = 0; i < 36500; i++) {
+            System.out.println("alive" + i);
+        }
+        System.out.println("goodbye world");
+    }
+}
+
+class God implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am alive");
+        }
+    }
+}
